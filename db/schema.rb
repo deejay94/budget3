ea86287bbe3ea65b461c9d074da00dc4d7050636
deejay94 <dahjoyner12@gmail.com>
2017-11-09 15:11:48 -0500

    Add column 'name'
    Add column 'name' with string type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171109200115) do
+ActiveRecord::Schema.define(version: 20171109201001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20171109200115) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "name"
     t.index ["user_id"], name: "index_budgets_on_user_id"
   end
 
